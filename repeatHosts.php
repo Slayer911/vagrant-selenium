9d620b3507fa97f26a5e61b0fd7aca525a269c96
rihalruslan <rihal@trafficterminal.com>
2017-10-12 14:41:21 +0300

    fix linux hosts file name

--- repeatHosts.php
+++ repeatHosts.php
@@ -2,7 +2,7 @@
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
     $hostFilePath = $_SERVER['SystemRoot'] . '\System32\drivers\etc\hosts';
 } else {
-    $hostFilePath = '\etc\hosts';
+    $hostFilePath = '/etc/hosts';
 }
 $hostData = file_get_contents($hostFilePath);
 $hostData = preg_replace('/127\.0\.0\.1/', '10.0.2.2', $hostData);
